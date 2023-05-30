dce318ca3ae266644ca99b757d8681c6c9a99481
ursmoscow <61637040+ursmoscow@users.noreply.github.com>
2023-05-30 23:50:59 +0300

    review

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -6,16 +6,11 @@ from posts.models import Comment, Group, Post
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(read_only=True,
                                           slug_field='username')
-    post = serializers.SlugRelatedField(read_only=True, slug_field='text')
+    post = serializers.SlugRelatedField(read_only=True, slug_field='id')
 
     class Meta:
         model = Comment
         fields = '__all__'
-
-    def to_representation(self, instance):
-        representation = super().to_representation(instance)
-        representation['post'] = instance.post.id
-        return representation
 
 
 class GroupSerializer(serializers.ModelSerializer):
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,5 +1,7 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken import views as auth_views
+
 
 from .views import CommentViewSet, GroupViewSet, PostViewSet
 
@@ -14,4 +16,5 @@ router.register('groups', GroupViewSet, basename='groups')
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('api-token-auth/', auth_views.obtain_auth_token)
 ]
